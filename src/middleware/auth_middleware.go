ae803e7be0f594650b7c2860938653e2055c81cb
rlevidev <leviizinho765@gmail.com>
2025-10-18 21:42:18 -0300

    ✨ feat(auth): adicionar middlewares de autenticação JWT
    - Implementa `AuthMiddleware` para proteger rotas, exigindo um token JWT válido no HEADER de autorização.
    - Adiciona `OptionalAuthMiddleware` para rotas que podem receber um token JWT, mas não o exigem para prosseguir.
    - Utiliza `jwtconfig` para extrair e validar tokens JWT.
    - Define `userID`, `userEmail` e `userName` no contexto do Gin após a validação bem-sucedida do token.
    - Retorna erros de não autorizado para tokens ausentes, malformados ou inválidos em `AuthMiddleware`.

---
+++ src/middleware/auth_middleware.go
@@ -0,0 +1,66 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	jwtconfig "github.com/rlevidev/oauth-go/src/config/jwt"
+	resterr "github.com/rlevidev/oauth-go/src/config/rest_err"
+)
+
+// Principal função de middleware para proteger rotas. Requer autenticação ( Bearer token )
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+
+		// Pega o HEADER de autorização
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader == "" {
+			restErr := resterr.NewUnauthorizedError("HEADER de autorização ausente")
+			ctx.JSON(restErr.Status, restErr)
+			ctx.Abort() // Interrompe a requisição
+			return
+		}
+
+		// Extrai o token do HEADER
+		tokenString := jwtconfig.ExtractTokenFromAuthHeader(authHeader)
+		if tokenString == "" {
+			restErr := resterr.NewUnauthorizedError("Token Bearer malformado")
+			ctx.JSON(restErr.Status, restErr)
+			ctx.Abort()
+			return
+		}
+
+		// Valida o token
+		claims, err := jwtconfig.ValidateToken(tokenString)
+		if err != nil {
+			restErr := resterr.NewUnauthorizedError("Token inválido ou expirado")
+			ctx.JSON(restErr.Status, restErr)
+			ctx.Abort()
+			return
+		}
+
+		// Adicionar informações do usuário ao contexto
+		ctx.Set("userID", claims.UserID)
+		ctx.Set("userEmail", claims.Email)
+		ctx.Set("userName", claims.Name)
+
+		// Continuar com a requisição
+		ctx.Next()
+	}
+}
+
+// Middleware opcional para verificar se o usuário está autenticado
+func OptionalAuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		authHeader := ctx.GetHeader("Authorization")
+		if authHeader != "" {
+			tokenString := jwtconfig.ExtractTokenFromAuthHeader(authHeader)
+			if tokenString != "" {
+				if claims, err := jwtconfig.ValidateToken(tokenString); err == nil {
+					ctx.Set("userID", claims.UserID)
+					ctx.Set("userEmail", claims.Email)
+					ctx.Set("userName", claims.Name)
+				}
+			}
+		}
+		ctx.Next()
+	}
+}
