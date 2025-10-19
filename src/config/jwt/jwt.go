0c178cf1971dcc899329405b9f9a4e6c7be9b830
rlevidev <leviizinho765@gmail.com>
2025-10-18 21:41:21 -0300

    ✨ feat(auth): adicionar funcionalidade de autenticação JWT
    - introduz um novo pacote `jwt` para gerenciar tokens JWT
    - inclui funções para gerar, validar e extrair tokens JWT
    - adiciona a dependência `github.com/golang-jwt/jwt/v5` para manipulação de JWTs
    - garante que a chave secreta JWT seja carregada de variáveis de ambiente
    - move a dependência `github.com/joho/godotenv` para uso direto no `go.mod`

---
+++ src/config/jwt/jwt.go
@@ -0,0 +1,81 @@
+package jwt
+
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/rlevidev/oauth-go/src/models"
+)
+
+var jwtSecret = []byte(getJWTSecret())
+
+func getJWTSecret() string {
+	secret := os.Getenv("JWT_SECRET")
+	if secret == "" {
+		log.Fatal("FATAL: JWT_SECRET não foi definida no arquivo .env. A aplicação não pode iniciar sem uma chave secreta.")
+
+	}
+	return secret
+}
+
+// Definir dados do token
+type Claims struct {
+	UserID               string `json:"user_id"`
+	Email                string `json:"email"`
+	Name                 string `json:"name"`
+	jwt.RegisteredClaims        // Expiração, emissão, etc.
+}
+
+// Gerar token
+func GenerateToken(ud *models.UserDomain) (string, error) {
+	expirationTime := time.Now().Add(24 * time.Hour) // Token válido por 24 horas
+
+	claims := &Claims{
+		UserID: ud.ID,
+		Email:  ud.Email,
+		Name:   ud.Name,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			Issuer:    "oauth-go",
+		},
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims) // Cria o token sem assinatura
+	tokenString, err := token.SignedString(jwtSecret)          // Assina o token
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+// Verificar token
+func ValidateToken(tokenString string) (*Claims, error) {
+	claims := &Claims{}
+
+	token, err := jwt.ParseWithClaims(tokenString, claims, func(token *jwt.Token) (interface{}, error) {
+		return jwtSecret, nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, jwt.ErrSignatureInvalid
+	}
+
+	return claims, nil
+}
+
+// Extrair token do cabeçalho
+func ExtractTokenFromAuthHeader(authHeader string) string {
+	const bearerPrefix = "Bearer "
+	if len(authHeader) > len(bearerPrefix) && authHeader[:len(bearerPrefix)] == bearerPrefix {
+		return authHeader[len(bearerPrefix):]
+	}
+	return ""
+}
